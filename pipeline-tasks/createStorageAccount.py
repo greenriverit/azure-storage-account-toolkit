b42ad721469e0fa09652a5f26f04fceb3146e220
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-07 12:06:28 -0700

    Update createStorageAccount.py

--- pipeline-tasks/createStorageAccount.py
+++ pipeline-tasks/createStorageAccount.py
@@ -14,6 +14,6 @@ depfunc.runShellCommand(createStorageAccountCommand)
 #Then create a storage container within the storage account  
 # #https://docs.microsoft.com/en-us/cli/azure/storage/container?view=azure-cli-latest
 # #https://docs.microsoft.com/en-us/cli/azure/storage/container?view=azure-cli-latest#az-storage-container-create
-#createStorageContainerCommand = "az storage container create -n " + storageContainerName + " --fail-on-exist --account-name " + storageAccountName
-#print("createStorageContainerCommand is: ", createStorageContainerCommand)
-#depfunc.runShellCommand(createStorageContainerCommand)  
+createStorageContainerCommand = "az storage container create -n " + storageContainerName + " --fail-on-exist --account-name " + storageAccountName
+print("createStorageContainerCommand is: ", createStorageContainerCommand)
+depfunc.runShellCommand(createStorageContainerCommand)  
